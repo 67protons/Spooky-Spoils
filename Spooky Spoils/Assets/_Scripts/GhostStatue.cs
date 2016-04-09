0ceee7c17b91128f06ed1a649a400cdff24d159c
Andrew <aschau@uci.edu>
2016-04-09 15:09:34 -0700

    Added Statue scripts and animators.

---
+++ Spooky Spoils/Assets/_Scripts/GhostStatue.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostStatue : MonoBehaviour {
+
+    public float speed = 1f;
+    public bool isChasing = false;
+    public bool activated = false;
+
+    private GameObject player;
+    private GazeAwareComponent _gazeAware;
+    private Animator anim;
+
+    void Awake()
+    {
+        _gazeAware = this.GetComponent<GazeAwareComponent>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        anim = this.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (activated)
+        {
+            checkState();
+            if (isChasing)
+            {
+                float step = speed * Time.deltaTime;
+                this.transform.position = Vector3.MoveTowards(this.transform.position, player.transform.position, step);
+            }
+        }
+    }
+
+    void checkState()
+    {
+        if (_gazeAware.HasGaze)
+        {
+            isChasing = false;
+            anim.SetBool("isAttacking", false);
+        }
+        else
+        {
+            isChasing = true;
+            anim.SetBool("isAttacking", true);
+            anim.Play("PlebStatueAttack");
+        }
+    }
+}
---
+++ Spooky Spoils/Assets/_Scripts/StatueTrigger.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatueTrigger : MonoBehaviour {
+    public GhostStatue owner;
+    private GazeAwareComponent _gazeAware;
+    void Awake()
+    {
+        _gazeAware = this.GetComponent<GazeAwareComponent>();
+    }
+
+    void Update()
+    {
+        checkState();
+    }
+    void checkState()
+    {
+        if (_gazeAware.HasGaze)
+        {
+            this.GetComponent<SpriteRenderer>().enabled = true;
+            this.GetComponent<Collider2D>().enabled = true;
+        }
+        else
+        {
+            this.GetComponent<SpriteRenderer>().enabled = false;
+            this.GetComponent<Collider2D>().enabled = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            owner.activated = true;
+        }
+    }
+}
