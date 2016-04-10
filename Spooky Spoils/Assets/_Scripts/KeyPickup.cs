89c53003335251b411f5b2a6fb7a232875181b36
Andrew <aschau@uci.edu>
2016-04-10 09:11:03 -0700

    fixed key detection. added StatueRoom2.unity

--- Spooky Spoils/Assets/_Scripts/KeyPickup.cs
+++ Spooky Spoils/Assets/_Scripts/KeyPickup.cs
@@ -5,10 +5,15 @@ public class KeyPickup : MonoBehaviour {
     public bool hasKey = false;
     // Use this for initialization
 	void Awake () {
+        this.GetComponent<Collider2D>().enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if (this.GetComponent<GazeAwareComponent>().HasGaze)
+        {
+            this.GetComponent<Collider2D>().enabled = true;
+        }
 	}
 
     void OnTriggerEnter2D(Collider2D other)
