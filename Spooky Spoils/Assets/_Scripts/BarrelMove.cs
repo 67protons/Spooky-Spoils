a45d0e749c836646ec8366a5f20bbbf7ab4a8024
hlimbo <limboh27@gmail.com>
2016-04-10 10:58:04 -0700

    Add sounds to BlockRoom

---
+++ Spooky Spoils/Assets/_Scripts/BarrelMove.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarrelMove : MonoBehaviour {
+
+    private AudioSource audioClip;
+    public bool isPushing;
+
+    void Start()
+    {
+        audioClip = GetComponent<AudioSource>();
+        isPushing = false;
+    }
+
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            audioClip.Play();
+        }
+
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        audioClip.Stop();
+    }
+
+
+}
