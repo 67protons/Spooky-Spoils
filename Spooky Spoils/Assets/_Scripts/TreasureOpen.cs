fb1e2533b4f8590f477d01ce53924a86e9c1bcbf
Andrew <aschau@uci.edu>
2016-04-10 10:06:35 -0700

    added TreasureRoom.unity

---
+++ Spooky Spoils/Assets/_Scripts/TreasureOpen.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreasureOpen : MonoBehaviour {
+    public Sprite opened;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            this.GetComponent<SpriteRenderer>().sprite = opened;
+    }
+}
