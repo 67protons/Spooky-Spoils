45f06f8ff31f34dae2798f0f6c9edcba19e2d666
hlimbo <limboh27@gmail.com>
2016-04-12 01:24:19 -0700

    Fonts folder added and improvements to menu ui scene

--- Spooky Spoils/Assets/_Scripts/MenuCursor.cs
+++ Spooky Spoils/Assets/_Scripts/MenuCursor.cs
@@ -4,7 +4,6 @@ using UnityEngine.UI;
 
 public class MenuCursor : MonoBehaviour {
 
-
     public GameObject PlayButton;
     public GameObject ExitButton;
     private Vector3 playOffset;
@@ -12,22 +11,29 @@ public class MenuCursor : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        //PlayPosition = new Vector3(61.4f,-79.5f,0.0f);
-        //ExitPosition = new Vector3(61.4f, -140.4f,0.0f);
-        //PlayPosition = Camera.main.WorldToViewportPoint(PlayPosition);
-        //ExitPosition = Camera.main.WorldToViewportPoint(ExitPosition);
         playOffset = PlayButton.transform.position;
         exitOffset = ExitButton.transform.position;
-        playOffset.x += 65.0f;
-        exitOffset.x += 65.0f;
         transform.position = playOffset;
     }
 	
 	// Update is called once per frame
-	void Update () {
-        if(Input.GetKeyDown(KeyCode.W))
+	void Update ()
+    {
+
+        playOffset = PlayButton.transform.position;
+        exitOffset = ExitButton.transform.position;
+
+        if (Input.GetKeyDown(KeyCode.Return))
         {
-            this.transform.position = playOffset;
+            if (transform.position == playOffset)
+                Application.LoadLevel("Foyer1");
+            if (transform.position == exitOffset)
+                Application.Quit();
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            this.transform.position =  playOffset;
             
         }
         if(Input.GetKeyDown(KeyCode.S))
@@ -35,10 +41,6 @@ public class MenuCursor : MonoBehaviour {
             this.transform.position = exitOffset;
         }
 
-        if(Input.GetKeyDown(KeyCode.Return) && transform.position == playOffset)
-            Application.LoadLevel("Foyer1");
-        if (Input.GetKeyDown(KeyCode.Return) && transform.position == exitOffset)
-            Application.Quit();
 	}
 
     public void TobiiOn(bool activated)
