b96c7fe13dc0c086acb57b7be5c606ca3d76a07b
eaho1 <eaho1@uci.edu>
2016-05-06 17:37:23 -0700

    Fixed Player Diagonal Animation

--- Spooky Spoils/Assets/_Scripts/Player.cs
+++ Spooky Spoils/Assets/_Scripts/Player.cs
@@ -4,92 +4,80 @@ using System.Collections;
 public class Player : MonoBehaviour {
     public float moveSpeed = 3f;
     private Animator anim;
-    private bool movingLeft, movingRight;
-    private bool movingUp, movingDown;
+    private Direction currentDirection = Direction.None;
+    private Direction lastDirection = Direction.None;
 
-    /*public override */void Start()
+    void Start()
     {
-        //base.Start();
         anim = this.GetComponent<Animator>();        
     }
 
-	/*public override */void Update () {
-        //base.Update();
-        //ManageState();
-        //Horizontal Movement
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            anim.SetBool("isWalking", true);
-            anim.Play("PlayerWalkLeft");
-            movingLeft = true;
-            movingRight = false;
-        }
-        if (Input.GetKeyUp(KeyCode.A))
-        {
-            //anim.SetBool("isWalking", false);
-            movingLeft = false;
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            anim.SetBool("isWalking", true);
-            anim.Play("PlayerWalkRight");
-            movingRight = true;
-            movingLeft = false;
-        }
-      
-        if (Input.GetKeyUp(KeyCode.D))
-        {
-            //anim.SetBool("isWalking", false);
-            movingRight = false;
-        }
+	void Update () {
 
-        //Vertical Movement
-        if (Input.GetKeyDown(KeyCode.W))
+        if (!(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.W)))
         {
-            anim.SetBool("isWalking", true);
-            anim.Play("PlayerWalkUp");
-            movingUp = true;
-            movingDown = false;
-        }
-        if (Input.GetKeyUp(KeyCode.W))
-        {
-            //anim.SetBool("isWalking", false);
-            movingUp = false;
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            anim.SetBool("isWalking", true);
-            anim.Play("PlayerWalkDown");
-            movingDown = true;
-            movingUp = false;
-        }
-        if (Input.GetKeyUp(KeyCode.S))
-        {
-            //anim.SetBool("isWalking", false);
-            movingDown = false;
-        }
-
-        //if (!(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W)))
-        if(!(movingLeft || movingRight || movingUp || movingDown))
             anim.SetBool("isWalking", false);
+            currentDirection = Direction.None;
+        }
 
+        if (Input.GetKey(KeyCode.A))
+        {
+            anim.SetBool("isWalking", true);
+            currentDirection = Direction.Left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            anim.SetBool("isWalking", true);
+            currentDirection = Direction.Right;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            anim.SetBool("isWalking", true);
+            currentDirection = Direction.Up;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            anim.SetBool("isWalking", true);
+            currentDirection = Direction.Down;
+        }
+
+        if (lastDirection != currentDirection)
+        {
+            switch (currentDirection)
+            {
+                case Direction.Left:
+                    anim.Play("PlayerWalkLeft");
+                    break;
+                case Direction.Right:
+                    anim.Play("PlayerWalkRight");
+                    break;
+                case Direction.Up:
+                    anim.Play("PlayerWalkUp");
+                    break;
+                case Direction.Down:
+                    anim.Play("PlayerWalkDown");
+                    break;
+            }
+        }
+
+        lastDirection = currentDirection;
 	}
 
     void FixedUpdate()
     {
-        if (movingLeft)
+        if (currentDirection == Direction.Left)
         {
             this.transform.Translate(new Vector2(-moveSpeed * Time.deltaTime, 0f));
         }
-        else if (movingRight)
+        else if (currentDirection == Direction.Right)
         {
             this.transform.Translate(new Vector2(moveSpeed * Time.deltaTime, 0f));
         }
-        if (movingUp)
+        else if (currentDirection == Direction.Up)
         {
             this.transform.Translate(new Vector2(0f, moveSpeed * Time.deltaTime));
         }
-        else if (movingDown)
+        else if (currentDirection == Direction.Down)
         {
             this.transform.Translate(new Vector2(0f, -moveSpeed * Time.deltaTime));
         }
